9c0c1b1472c0fc38188a485df3293e9a7c00823c
Sergio Zavala <106635253+sezavala@users.noreply.github.com>
2026-06-17 10:06:28 -0700

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] letters = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            letters[s.charAt(i) - 'a']++;
+            letters[t.charAt(i) - 'a']--;
+        }
+
+        for (int num: letters) {
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
